eeb980fd3f42de10b1f47844ee92d716b76b12ec
CherryGarden <alchemist.fumi@gmail.com>
2025-11-04 12:31:13 +0900

    チュートリアルの敵修正

--- Assets/script/TutorialEnemy.cs
+++ Assets/script/TutorialEnemy.cs
@@ -3,22 +3,24 @@ using UnityEngine;
 public class TutorialEnemy : MonoBehaviour
 {
 	public float fallSpeed = 5f; // 落下スピード
-	//public float moveSpeed = 3f; // プレイヤー追跡スピード
+								 //public float moveSpeed = 3f; // プレイヤー追跡スピード
 	public float frontOffset = 20; //追跡する位置（プレイヤーの前方）
 	public float radius = 10f; //半径
 	public float rotateSpeed = 2f; //角速度
 	public Transform player;
-	
+
 	private bool hasLanded = false;
 	private Vector3 offset;
 	private Transform target;
 	private Vector3 circleCenter; //回転の中心
 	private float angle = 0f;
+	private Transform cam;
 
 	private void Start()
 	{
 		// シーン上のプレイヤーを自動で取得
 		player = GameObject.Find("playerTest").transform;
+		cam = Camera.main?.transform;
 
 		if (player != null)
 		{
@@ -29,11 +31,12 @@ public class TutorialEnemy : MonoBehaviour
 		{
 			Debug.LogError("Player not found in scene!");
 		}
+
 	}
 
 	private void Update()
 	{
-if (player == null) return;
+		if (player == null) return;
 		if (!hasLanded)
 		{
 			// 下に移動
@@ -45,7 +48,7 @@ if (player == null) return;
 			if (transform.position.y <= cameraY) // ← 地面の高さに合わせる
 			{
 				hasLanded = true;
-				circleCenter = player.position + player.forward * frontOffset;
+
 				angle = Random.Range(0f, Mathf.PI * 2f);
 				Debug.Log("Start Circle Motion");
 			}
@@ -53,12 +56,20 @@ if (player == null) return;
 		}
 		else
 		{
+			//カメラを中心に円運動
 			angle += rotateSpeed * Time.deltaTime;
-			float x = Mathf.Cos(angle) * radius;
-			float z = Mathf.Sin(angle) * radius;
-			// 追従しつつ円運動
-			transform.position = circleCenter + new Vector3(x,0,z);
+
+			//座標計算
+			Vector3 circlePos = cam.position
+			+ cam.forward * frontOffset //カメラの前方
+			+ cam.right * Mathf.Cos(angle) * radius //水平方向
+			+ cam.up * Mathf.Sin(angle) * radius; //垂直方向
+
+			transform.position = circlePos;
+			//プレイヤーの方向を向く
 			transform.LookAt(player);
+
+
 		}
 	}
 }
