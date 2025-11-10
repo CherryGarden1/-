6fc850b7900ec0ffafba3f0cbdff60322fe5d9ef
CherryGarden <alchemist.fumi@gmail.com>
2025-11-10 15:05:53 +0900

    爆破仮

--- Assets/script/Bulast.cs
+++ Assets/script/Bulast.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class Bulast : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private float bulletSpeed;
+	[SerializeField]
+	private Rigidbody rb;
+	[SerializeField]
+	GameObject FirePoint;
+	public float explosionRadius = 10f;  // 最初の爆発範囲
+	public float chainDelay = 0.2f;      // 連鎖までの遅延
+	public int maxChains = 5;            // 最大連鎖回数
+	public float Exdamage = 50f;           // 連鎖爆発用ダメージ
+			
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Enemy"))
+		{
+			//最初の敵を爆破する
+			StartCoroutine(TriggerChainExplosion(other.transform, 0));
+			Destroy(gameObject);
+		}
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private IEnumerator TriggerChainExplosion(Transform originEnemy, int chainLevel)
+	{
+		if (chainLevel > maxChains) yield break;
+
+		// 爆発エフェクト
+		Instantiate(Resources.Load<GameObject>("ExplosionEffect"), originEnemy.position, Quaternion.identity);
+
+		// 敵を破壊 or ダメージ
+		EnemyBase enemy = originEnemy.GetComponent<EnemyBase>();
+		if (enemy != null)
+		{
+			enemy.TakeDamage(Exdamage);
+		}
+
+		// 次の連鎖を探索
+		Collider[] hits = Physics.OverlapSphere(originEnemy.position, explosionRadius);
+		foreach (Collider hit in hits)
+		{
+			if (hit.CompareTag("Enemy") && hit.transform != originEnemy)
+			{
+				yield return new WaitForSeconds(chainDelay);
+				StartCoroutine(TriggerChainExplosion(hit.transform, chainLevel + 1));
+			}
+		}
+	}
 }
--- Assets/script/EnemyBase.cs
+++ Assets/script/EnemyBase.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class EnemyBase : MonoBehaviour
 {
 	public int hp = 10;
+	public GameObject explosionPrefab;
 	public virtual void TakeDamage(int damage)
 	{
 		hp -= damage;
 		if (hp <= 0) Destroy(gameObject);
+	}
+	void Explode()
+	{
+		if (explosionPrefab)
+		{ 
+			Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+		}
+		Destroy(gameObject);
 	}
 }
